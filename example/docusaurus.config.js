91f6e12b3438a549d75988c12a69315288061bba
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-07-29 02:12:55 +0200

    Update settings.. again

--- example/docusaurus.config.js
+++ example/docusaurus.config.js
@@ -114,7 +114,7 @@ module.exports = {
         docsPath: 'example/docs',
         github: {
           clientId: 'f46b54ebe4b6254c91bd',
-          tokenUri: 'https://lit-springs-95362.herokuapp.com/authorize/'
+          tokenUri: 'https://secret-stream-27092.herokuapp.com/authorize/'
         }
       }
     ]
