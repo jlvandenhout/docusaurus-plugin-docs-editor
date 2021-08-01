f41906609a013a33b4d2d1c5dfb3237e2331847f
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-08-01 23:52:01 +0200

    Correct spacing of menu item

--- src/theme/EditorMenu.js
+++ src/theme/EditorMenu.js
@@ -8,7 +8,7 @@ const headingLevels = [1, 2, 3, 4, 5, 6]
 
 const EditorGroup = ({children}) => {
   return (
-    <div className={clsx('editor__group', 'padding-horiz--xs')}>
+    <div className={clsx('editor__group', 'margin-vert--sm', 'padding-horiz--xs')}>
       {children}
     </div>
   )
@@ -81,79 +81,83 @@ export default function EditorMenu({ editor, save, submit, syncing, className })
       <Head>
         <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'></link>
       </Head>
-      <div className={clsx('editor__menu', 'padding-vert--sm', className)}>
-        <EditorGroup>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().undo().run()}>
-            undo
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().redo().run()}>
-            redo
-          </EditorIcon>
-        </EditorGroup>
-        <EditorGroup>
-          <select
-            className={clsx('editor__select', 'margin-horiz--xs')}
-            value={checkFontStyle()}
-            onChange={changeFontStyle}
-          >
-            <option hidden disabled value=''></option>
-            <option value='paragraph'>Normal text</option>
-            {headingLevels.map((level) => {
-              return <option key={level} value={level}>{`Heading ${level}`}</option>
-            })}
-          </select>
-        </EditorGroup>
-        <EditorGroup>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleBold().run()} name='bold'>
-            format_bold
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleItalic().run()} name='italic'>
-            format_italic
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleCode().run()} name='code'>
-            code
-          </EditorIcon>
-        </EditorGroup>
-        <EditorGroup>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleBulletList().run()} name='bulletList'>
-            format_list_bulleted
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleOrderedList().run()} name='orderedList'>
-            format_list_numbered
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleCodeBlock().run()} name='codeBlock'>
-            code
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().toggleBlockquote().run()} name='blockquote'>
-            format_quote
-          </EditorIcon>
-        </EditorGroup>
-        <EditorGroup>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().setHorizontalRule().run()}>
-            horizontal_rule
-          </EditorIcon>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().setHardBreak().run()}>
-            keyboard_return
-          </EditorIcon>
-        </EditorGroup>
-        <EditorGroup>
-          <EditorIcon editor={editor} action={() => editor.chain().focus().unsetAllMarks().clearNodes().run()}>
-            format_clear
-          </EditorIcon>
-        </EditorGroup>
-        <EditorGroup>
-          <button className='editor__button margin-horiz--xs padding-horiz--sm' disabled={syncing} onClick={onSave}>
-            {syncing ?
-              <span className='editor__loading material-icons'>autorenew</span>
-            :
-              <span className='material-icons'>file_download</span>
-            }
-             Save
-          </button>
-          <button className='editor__button margin-horiz--xs padding-horiz--sm' disabled={syncing} onClick={onSubmit}>
-            <span className='material-icons'>file_upload</span> Submit
-          </button>
-        </EditorGroup>
+      <div className={clsx('editor__menu', className)}>
+        <div className={clsx('editor__submenu')}>
+          <EditorGroup>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().undo().run()}>
+              undo
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().redo().run()}>
+              redo
+            </EditorIcon>
+          </EditorGroup>
+          <EditorGroup>
+            <select
+              className={clsx('editor__select', 'margin-horiz--xs')}
+              value={checkFontStyle()}
+              onChange={changeFontStyle}
+            >
+              <option hidden disabled value=''></option>
+              <option value='paragraph'>Normal text</option>
+              {headingLevels.map((level) => {
+                return <option key={level} value={level}>{`Heading ${level}`}</option>
+              })}
+            </select>
+          </EditorGroup>
+          <EditorGroup>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleBold().run()} name='bold'>
+              format_bold
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleItalic().run()} name='italic'>
+              format_italic
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleCode().run()} name='code'>
+              code
+            </EditorIcon>
+          </EditorGroup>
+          <EditorGroup>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleBulletList().run()} name='bulletList'>
+              format_list_bulleted
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleOrderedList().run()} name='orderedList'>
+              format_list_numbered
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleCodeBlock().run()} name='codeBlock'>
+              code
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().toggleBlockquote().run()} name='blockquote'>
+              format_quote
+            </EditorIcon>
+          </EditorGroup>
+          <EditorGroup>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().setHorizontalRule().run()}>
+              horizontal_rule
+            </EditorIcon>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().setHardBreak().run()}>
+              keyboard_return
+            </EditorIcon>
+          </EditorGroup>
+          <EditorGroup>
+            <EditorIcon editor={editor} action={() => editor.chain().focus().unsetAllMarks().clearNodes().run()}>
+              format_clear
+            </EditorIcon>
+          </EditorGroup>
+        </div>
+        <div className={clsx('editor__submenu')}>
+          <EditorGroup>
+            <button className='editor__button margin-horiz--xs padding-horiz--sm' disabled={syncing} onClick={onSave}>
+              {syncing ?
+                <span className='editor__loading material-icons'>autorenew</span>
+              :
+                <span className='material-icons'>file_download</span>
+              }
+              Save
+            </button>
+            <button className='editor__button margin-horiz--xs padding-horiz--sm' disabled={syncing} onClick={onSubmit}>
+              <span className='material-icons'>file_upload</span> Submit
+            </button>
+          </EditorGroup>
+        </div>
       </div>
     </>
   )
