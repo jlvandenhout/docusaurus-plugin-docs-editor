8c761e91b6d394b063e2e961c363cf8239c92ec3
Joshua Chen <sidachen2003@gmail.com>
2021-09-03 04:40:02 +0800

    Add ESLint & Prettier (#37)
    * Add eslint
    
    * Add CI
    
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+  plugins: ['react', 'react-hooks'],
+};
--- src/theme/EditorCodeBlock.js
+++ src/theme/EditorCodeBlock.js
@@ -1,33 +1,43 @@
-import React, { useState } from 'react'
-import { NodeViewWrapper, NodeViewContent } from '@tiptap/react'
+import React, { useState } from 'react';
+import { NodeViewWrapper, NodeViewContent } from '@tiptap/react';
 
-import clsx from 'clsx'
+import clsx from 'clsx';
 
-export default ({ node: { attrs: { language: defaultLanguage } }, updateAttributes, extension }) => {
-  const [language, setLanguage] = useState(defaultLanguage)
+export default ({
+  node: {
+    attrs: { language: defaultLanguage },
+  },
+  updateAttributes,
+  extension,
+}) => {
+  const [language, setLanguage] = useState(defaultLanguage);
 
   const updateLanguage = (language) => {
-    setLanguage(language)
-    updateAttributes({ language })
-  }
+    setLanguage(language);
+    updateAttributes({ language });
+  };
 
   return (
-    <NodeViewWrapper className='codeblock'>
+    <NodeViewWrapper className="codeblock">
       <select
-        className='codeblock__language'
+        className="codeblock__language"
         contentEditable={false}
         defaultValue={defaultLanguage}
-        onChange={event => updateLanguage(event.target.value)}
-      >
+        onChange={(event) => updateLanguage(event.target.value)}>
         <option value>auto</option>
         <option disabled>---</option>
         {extension.options.lowlight.listLanguages().map((lang, index) => (
-          <option key={index} value={lang}>{lang}</option>
+          <option key={index} value={lang}>
+            {lang}
+          </option>
         ))}
       </select>
-      <pre className='codeblock__code'>
-        <NodeViewContent as='code' className={clsx(language ? `language-${language}` : '')} />
+      <pre className="codeblock__code">
+        <NodeViewContent
+          as="code"
+          className={clsx(language ? `language-${language}` : '')}
+        />
       </pre>
     </NodeViewWrapper>
-  )
-}
+  );
+};
